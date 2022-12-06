5eb1dfb31411f6858e5d6773a5bc43744d4737d6
Mattis Zheng <mattiszheng@gmail.com>
2022-12-06 17:35:37 +0800

    update javascript, set offset to offsetHeight. remove dashboard from work section

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.querySelector('header').style.top = '';
+    document.querySelector('header').style.top = '0';
   } else {
-    document.querySelector('header').style.top = `-50px`;
+    document.querySelector('header').style.top = `-${document.querySelector('header').offsetHeight}px`;
   }
   prevScrollpos = currentScrollPos;
 };
